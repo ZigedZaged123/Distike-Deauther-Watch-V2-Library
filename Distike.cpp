d9362a2a85c305d02ac2a86ec9df93c4e7d826fd
ZigedZaged123 <78444659+ZigedZaged123@users.noreply.github.com>
2021-04-04 07:47:41 -0400

    Create Distike.cpp

---
+++ Distike.cpp
@@ -0,0 +1,18 @@
+#include "Arduino.h"
+#include "Distike.h"
+
+Distike::Distike(){
+
+void Distike::startUp() {
+  DisplaySH1106_128x64_I2C display(-1, {-1, 0x3C, OLED_SCL, OLED_SDA, -1});
+  display.begin();
+}
+
+void Distike::troubleShootScreen() {
+  display.setFixedFont(ssd1306xled_font6x8);
+  display.fill(0x00);
+  display.clear();
+
+  display.printFixed(0, 0, "Screen Troubleshoot Test", STYLE_NORMAL);
+}
+}
