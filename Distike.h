f73d257fcdcca341443e3f6f00e15f58543e7b93
ZigedZaged123 <78444659+ZigedZaged123@users.noreply.github.com>
2021-04-04 07:45:48 -0400

    Create Distike.h

---
+++ Distike.h
@@ -0,0 +1,32 @@
+#indef Distike_h
+#define Distike_h
+#include "Arduino.h"
+#include "Bounce2.h"
+#include "lcdgfx.h"
+#include "lcdgfx_gui.h"
+#include "ESP8266WiFi.h"
+#include "WiFi.h"
+
+
+class DISTIKE {
+
+    private:
+
+    #define OLED_SDA 5
+    #define OLED_SCL 4
+    #define DISTIKE_LED 15
+    #define BUTTON_UP 12
+    #define BUTTON_DOWN 13
+    #define BUTTON_IN 14
+    #define DISTIKE_LASER 16
+    #define SIDE_BUTTON_1 0
+    #define SIDE_BUTTON_2 RST
+
+    public:
+        DISTIKE();
+        void startUp();
+        void troubleShootScreen();
+
+}
+
+#endif
